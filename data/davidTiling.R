a054d7bdd5574da1929ce43ef28af30eaf5f5a4f
Hugo Gruson <git@hugogruson.fr>
2026-08-17 15:32:40 +0200

    Add deprecation error in data("davidTiling")

---
+++ data/davidTiling.R
@@ -0,0 +1,7 @@
+delayedAssign(
+  "davidTiling",
+  stop(
+    "The davidTiling dataset is no longer included in this package. ",
+    "Please use davidTiling::getDavidTilingData() instead."
+  )
+)
